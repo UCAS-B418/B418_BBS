03b9f948cce5e75cc0330c03000eac001096af10
Alittlefan <fan1135152063@gmail.com>
2024-07-05 03:57:09 +0000

    food

--- vueUser/src/views/Food.vue
+++ vueUser/src/views/Food.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="food">
-    <h1>今日美食</h1>
-    <div class="price-buttons">
-      <button @click="filterDishes('10元区')">十元区</button>
-      <button @click="filterDishes('6元区')">六元区</button>
-      <button @click="filterDishes('4元区')">四元区</button>
+  <div class="food-container">
+    <div class="category-buttons">
+      <button @click="setCategory('ten')">十元区</button>
+      <button @click="setCategory('six')">六元区</button>
+      <button @click="setCategory('four')">四元区</button>
     </div>
-    <div class="dishes">
-      <div v-for="dish in filteredDishes" :key="dish.id" class="dish">
-        <img :src="dish.image" :alt="dish.name" class="dish-image">
-        <p class="dish-name">{{ dish.name }}</p>
+    <div class="food-items">
+      <div v-for="food in filteredFoods" :key="food.name" class="food-item">
+        <img :src="food.image" :alt="food.name" />
+        <p>{{ food.name }}</p>
       </div>
     </div>
   </div>
@@ -19,71 +18,62 @@
 export default {
   data() {
     return {
-      selectedCategory: '10元区',
-      dishes: [
-        { id: 1, name: '红烧肉', category: '10元区', image: 'path/to/image1.jpg' },
-        { id: 2, name: '宫保鸡丁', category: '10元区', image: 'path/to/image2.jpg' },
-        { id: 3, name: '鱼香茄子', category: '10元区', image: 'path/to/image3.jpg' },
-        { id: 4, name: '炒青菜', category: '6元区', image: 'path/to/image4.jpg' },
-        { id: 5, name: '西红柿炒鸡蛋', category: '6元区', image: 'path/to/image5.jpg' },
-        { id: 6, name: '麻婆豆腐', category: '6元区', image: 'path/to/image6.jpg' },
-        { id: 7, name: '蒸蛋', category: '4元区', image: 'path/to/image7.jpg' },
-        { id: 8, name: '白菜豆腐汤', category: '4元区', image: 'path/to/image8.jpg' },
-        { id: 9, name: '清炒豆芽', category: '4元区', image: 'path/to/image9.jpg' }
-      ]
+      category: 'ten',
+      foods: [
+        { name: '红烧肉', category: 'ten', image: '../../images/hongshaorou.png' },
+        { name: '葱爆羊肉', category: 'ten', image: '../../images/cbyr.png' },
+        { name: '冬瓜排骨汤', category: 'ten', image: '/images/dgpgt.jpg' },
+        { name: '食物D', category: 'six', image: '/images/imageD.jpg' },
+        { name: '食物E', category: 'six', image: '/images/imageE.jpg' },
+        { name: '食物F', category: 'six', image: '/images/imageF.jpg' },
+        { name: '食物G', category: 'four', image: '/images/imageG.jpg' },
+        { name: '食物H', category: 'four', image: '/images/imageH.jpg' },
+        { name: '食物I', category: 'four', image: '/images/imageI.jpg' },
+      ],
     };
   },
   computed: {
-    filteredDishes() {
-      return this.dishes.filter(dish => dish.category === this.selectedCategory);
-    }
+    filteredFoods() {
+      return this.foods.filter(food => food.category === this.category);
+    },
   },
   methods: {
-    filterDishes(category) {
-      this.selectedCategory = category;
-    }
-  }
+    setCategory(category) {
+      this.category = category;
+    },
+  },
 };
 </script>
 
 <style scoped>
-.food {
-  padding: 20px;
+.food-container {
   text-align: center;
 }
 
-.price-buttons {
+.category-buttons {
   margin-bottom: 20px;
 }
 
-.price-buttons button {
+.category-buttons button {
   margin: 0 10px;
   padding: 10px 20px;
-  font-size: 16px;
   cursor: pointer;
 }
 
-.dishes {
+.food-items {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
 
-.dish {
-  width: 30%;
+.food-item {
+  width: 25%;
   margin: 10px;
   text-align: center;
 }
 
-.dish img {
+.food-item img {
   width: 100%;
   height: auto;
-  border-radius: 8px;
-}
-
-.dish-name {
-  margin-top: 10px;
-  font-size: 18px;
-  font-weight: bold;
 }
 </style>
